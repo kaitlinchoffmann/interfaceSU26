4ace77ccbf24f1c031478d576958d84ff3b3adbb
kaitlinchoffmann <kaitlinchoffmann@gmail.com>
2026-06-09 03:06:49 -0400

    Create User model and route file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+app.use(express.json())
 
 const userRoutes = require("./server/routes/user")
 
